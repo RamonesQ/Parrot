e4bcfa392555cef9b54fc7e37fd2d547bfbb272f
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-06-12 19:44:15 -0300

    Rotas de Post

--- API/src/routes/index.ts
+++ API/src/routes/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { AuthController } from '../domain/Auth/controller/Auth.controller';
+import PostController from '../domain/Posts/controllers/postController';
 import { UsersController } from '../domain/Users/controllers/users.controller';
+const Auth = require('../middlewares/auth');
 
 
 export const routes = express.Router();
@@ -11,5 +13,9 @@ routes.post('/user', UsersController.postUser);
 routes.post('/login', AuthController.login);
 routes.put('/user/:id', UsersController.putUser);
 routes.delete('/user/:id', UsersController.deleteUser)
+
+routes.get('/post', PostController.getAllPosts);
+routes.post('/post', PostController.postPosts);
+
 
 
