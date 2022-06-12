f1e2d9909668a4bcf9ebaf057fedd5b8ccd98425
unknown <eniofilho76@gmail.com>
2022-06-12 09:51:57 -0300

    projeto front

---
+++ WEB/src/App.tsx
@@ -0,0 +1,15 @@
+import { Fragment, useState } from 'react'
+import { Navbar } from 'react-bootstrap'
+import React from 'react'
+
+function App () {
+  
+  return (
+
+    <div>
+      <Navbar  variant='light'/>
+      </div>
+  )
+}
+
+export default App
---
+++ WEB/src/assets/Modal/MLogin.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "react-bootstrap";
+
+
+import React from 'react'
+
+const MLogin = () => {
+  return (
+    <div>
+        <Navbar  variant='light'>TEste</Navbar>
+    </div>
+  )
+}
+
+export default MLogin
