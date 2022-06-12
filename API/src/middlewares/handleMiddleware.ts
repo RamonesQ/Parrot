675b2471de71ec926f67ad8ce971ae78b7b2f1b3
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-06-12 19:44:32 -0300

    Import

--- API/src/middlewares/handleMiddleware.ts
+++ API/src/middlewares/handleMiddleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response} from "express";
 import { ValidationError } from "express-validation";
 
+
 export default function (err: Error, req: Request, res: Response, next: NextFunction) {
   if (err instanceof ValidationError) {
    return res.status(err.statusCode).json(err);
