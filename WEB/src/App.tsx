bb2e6d49c6249348afb06127159056940841161d
unknown <eniofilho76@gmail.com>
2022-06-13 15:59:45 -0300

    pagina de cadastro realizada

--- WEB/src/App.tsx
+++ WEB/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg'
 import './App.css'
 import Login from './components/Login/Login'
 import Perfil from './components/Perfil/Perfil'
+import Cadastro from './components/Cadastro/Cadastro'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,8 @@ function App() {
   return (
     <div className="App">
       {/* <Login /> */}
-      <Perfil />
+      {/* <Perfil /> */}
+      <Cadastro />
     </div>
   )
 }
---
+++ WEB/src/components/Cadastro/Cadastro.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Fragment } from 'react';
+import { Button, Container, Form } from "react-bootstrap";
+import "./Cadastro.css"
+import Logo from "../../assets/Imagens/logo.png"
+
+
+
+
+const Cadastro = () => {
+  return (
+    <div>
+      <Container>
+        <img className='logo' src={Logo} alt="Logo-Pagina-Login" />
+        <h5 className='login'>CADASTRO</h5>
+        <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail"style={{padding: "1% 15%"}}>
+    <Form.Control type="email" placeholder="nome" style={{textAlign: "center" }}/>
+     </Form.Group>
+     <Form.Group className="mb-3" controlId="formBasicEmail"style={{padding: "1% 15%"}}>
+    <Form.Control type="email" placeholder="email" style={{textAlign: "center" }}/>
+     </Form.Group>
+     <Form.Group className="mb-3" controlId="formBasicEmail"style={{padding: "1% 15%"}}>
+    <Form.Control type="email" placeholder="senha" style={{textAlign: "center" }}/>
+     </Form.Group>
+     <Form.Group className="mb-3" controlId="formBasicEmail"style={{padding: "1% 15%"}}>
+    <Form.Control type="email" placeholder="confirmar senha" style={{textAlign: "center" }}/>
+     </Form.Group>
+     <Form.Group className="mb-3" controlId="formBasicEmail"style={{padding: "1% 15%"}}>
+    <Form.Control type="email" placeholder="unidade/apartamento" style={{textAlign: "center" }}/>
+     </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicPassword" style={{padding: "1% 15%"}}>
+        <Form.Control type="password" placeholder="link da foto" style={{textAlign: "center" }} />
+  </Form.Group>
+    <Button className='botao' type="submit" style={{background: "#76BB4C", padding: "1% 30%", color: "black"}}>
+    entrar
+  </Button>
+</Form>
+      
+            </Container>
+      </div>
+  )
+}
+
+export default Cadastro
