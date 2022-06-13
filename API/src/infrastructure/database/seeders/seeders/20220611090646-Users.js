cb453ef5448c6deda1a72765a45136e8b1bf9710
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-06-12 22:26:58 -0300

    Seeders Post

--- API/src/infrastructure/database/seeders/seeders/20220611090646-Users.js
+++ API/src/infrastructure/database/seeders/seeders/20220611090646-Users.js
@@ -1,26 +1,34 @@
 'use strict';
 
+const { faker } = require('@faker-js/faker');
+
 let seeders = [];
+
+for (let i = 0; i < 10; i++) {
+  seeders.push(
+    {
+
+      name: faker.name.firstName(),
+      email: faker.internet.email(),
+      apartment: 1,
+      password: '123456',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  )
+}
 
 
 
 module.exports = {
   async up(queryInterface, Sequelize) {
 
-    await queryInterface.bulkInsert('user', [{
-      name: 'Teste',
-      email: 'Teste@gmail.com',
-      apartment: 1,
-      password: 'teste',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
-
+    await queryInterface.bulkInsert('user', seeders);
   },
 
   async down(queryInterface, Sequelize) {
-   
-     await queryInterface.bulkDelete('user', null, {});
-     
+
+    await queryInterface.bulkDelete('user', null, {});
+
   }
 };
