682af30776e0dce24eb802c3c9136fdcb9468bc9
unknown <eniofilho76@gmail.com>
2022-06-13 15:24:57 -0300

    comprarilhando o repo atualizado para o israel

--- WEB/src/App.tsx
+++ WEB/src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Login from './components/Login/Login'
+import Perfil from './components/Perfil/Perfil'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <Perfil />
     </div>
   )
 }
