1b7ceb70eb669b39e256dc004c1e301f10809e7b
RamonoesQ <ramonqueirozs@gmail.com>
2022-06-14 18:47:06 -0300

    novo teste

--- API/src/domain/Users/controllers/users.controller.spec.ts
+++ API/src/domain/Users/controllers/users.controller.spec.ts
@@ -4,4 +4,7 @@ describe('metodo getAllUser()', () => {
     test('deve ser do type function', () => {
         expect(typeof UsersController.getAllUser).toBe('function')
     }) 
+    test('deve ser do type function', async () => {
+        expect(await UsersController.getAllUser()).toBe()
+    })
 })
