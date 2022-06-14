994fcec4f1754412c24cbdedaf823b7edd04816e
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-06-14 18:01:46 -0300

    Cors

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import handleMiddleware from './middlewares/handleMiddleware';
 import {routes} from './routes';
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 app.use(handleMiddleware);
 
