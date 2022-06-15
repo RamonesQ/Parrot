dfa4580977e94472ae75f9eeff8a4cc884071491
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-06-15 08:59:47 -0300

    Função de logout

--- WEB/src/components/Header/Header.tsx
+++ WEB/src/components/Header/Header.tsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import { useContext } from "react";
 import { Navbar, Container } from "react-bootstrap";
 import LogoPerfil from "../../assets/Imagens/LogoPerfil.png"
-import Icone from "../../assets/Imagens/Icone.png"
-import  "./Header.css"
+import { AuthContext } from '../../contexts/auth';
+import "./Header.css"
 
 const Header = () => {
+  const { logout } = useContext(AuthContext);
+  const handleLogout = () => {
+    logout();
+  }
   return (
     <div className='teste'>
-    <Navbar className='nav' bg="light" style={{boxShadow: "0px 4px 6px #d1cfcf", }}>
-         <Container>
-        <img src={LogoPerfil} alt="Logo-perfil" />
-      <Navbar.Brand href="#home" style={{color: "#76BB4C"}}>Olá Usuário <a id="barra" style={{color: "#6033AA"}}>     Sair</a></Navbar.Brand>
-    </Container>
-  </Navbar>
+      <Navbar className='nav' bg="light" style={{ boxShadow: "0px 4px 6px #d1cfcf", }}>
+        <Container>
+          <img src={LogoPerfil} alt="Logo-perfil" />
+          <Navbar.Brand href="#home" style={{ color: "#76BB4C" }}>Olá Usuário <button onClick={handleLogout} id="barra" style={{ color: "#6033AA" }}>     Sair</button></Navbar.Brand>
+        </Container>
+      </Navbar>
 
-        </div>
+    </div>
   )
 }
 
