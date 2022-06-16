c130c77bf9165a6bd6b6c2bb3d8702a1b2ffdb6a
RamonoesQ <ramonqueirozs@gmail.com>
2022-06-16 17:23:20 -0300

    criado testes de rotas

--- API/src/domain/Users/controllers/users.controller.spec.ts
+++ API/src/domain/Users/controllers/users.controller.spec.ts
@@ -6,6 +6,33 @@ describe('metodo getAllUser()', () => {
     test('deve ser do type function', () => {
         expect(typeof UsersController.getAllUser).toBe('function')
     })
+    test('deve ser do type function', () => {
+        expect(typeof UsersController.getUserById).toBe('function')
+    })
+    test('deve ser do type function', () => {
+        expect(typeof UsersController.deleteUser).toBe('function')
+    })
+    test('deve ser do type function', () => {
+        expect(typeof UsersController.postUser).toBe('function')
+    })
+    test('deve ser do type function', () => {
+        expect(typeof UsersController.putUser).toBe('function')
+    })
 })
 
+describe('No controller, ao executar função', () => {
+    
+describe('create', () => {
+        test('em caso de sucesso, deve retornar 201', async () => {
+            const res = await supertest(app)
+                .post('/user')
+                .send({
+                    name: "meu nome",
+                    email: "me@email.com",
+                    apartment: 2,
+                    password: "senha"
+                })
 
+            expect(res.status).toBe(201)
+        })
+})})
--- API/src/routes/index.spec.ts
+++ API/src/routes/index.spec.ts
@@ -6,17 +6,14 @@ import app from "../index"
 describe('Teste de rotas get user ()', () => {
     test('Recuperando todos os usuarios com get /user', async () => {
         const res = await supertest(app).get('/user')
-        console.log(res.body)
         expect(res.status).toBe(200)
     })
     test('Recuperando os usuarios com paginaçao get /user?page=2', async () => {
         const res = await supertest(app).get('/user?page=2')
-        console.log(res.body)
         expect(res.status).toBe(200)
     })
     test('Recuperando usuarios por ID get /user/2', async () => {
         const res = await supertest(app).get('/user/2')
-        console.log(res.body)
         expect(res.status).toBe(200)
     })
     
