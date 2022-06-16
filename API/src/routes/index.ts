1871dbb8be5fb7b480098cb67f2dd7c13d2cfa08
RamonoesQ <ramonqueirozs@gmail.com>
2022-06-16 20:33:55 -0300

    Revert "separação de rotas"
    This reverts commit dd49862a307255c6994e5b9981815975314551b6.

---
+++ API/src/routes/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import { AuthControler } from '../domain/Auth/authcontroller';
+import PostController from '../domain/Posts/controllers/postController';
+import { PostValidation } from '../domain/Posts/validations';
+import { UsersController } from '../domain/Users/controllers/users.controller';
+import { UserValidation } from '../domain/Users/validations';
+const Auth = require('../middlewares/auth');
+
+
+export const routes = express.Router();
+
+routes.get('/user', UsersController.getAllUser);
+routes.get('/user/:id', UserValidation.getOne, UsersController.getUserById);
+routes.post('/user', UserValidation.create, UsersController.postUser);
+routes.put('/user/:id', UserValidation.update, UsersController.putUser);
+routes.delete('/user/:id', Auth, UserValidation.destroy, UsersController.deleteUser)
+
+routes.get('/post', PostController.getAllPosts);
+routes.get('/post/:id', PostValidation.getOne, PostController.getPostsById);
+routes.post('/post', PostValidation.create, PostController.postPosts);
+routes.put('/post/:id', PostValidation.update, PostController.putPosts);
+routes.delete('/post/:id', PostValidation.destroy, PostController.deletePost);
+routes.get('/post/:id', PostController.contarPost)
+
+routes.post('/login', AuthControler.login);
--- API/src/routes/loginRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { AuthControler } from '../domain/Auth/authcontroller';
-
-const routes = express.Router();
-
-
-routes.post('/login', AuthControler.login);
-
-export default routes;
