b893b71111d1370f111cfb1d6d394b7c5ac8e1f3
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-06-16 18:13:00 -0300

    Componente Editar

---
+++ WEB/src/components/Editar/Editar.tsx
@@ -0,0 +1,63 @@
+import { Button, Container, Form } from "react-bootstrap";
+import "./Editar.css"
+import Logo from "../../assets/Imagens/logo.png"
+import Background from "../../assets/Imagens/background.png"
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import Api from "../../api"
+
+
+
+const Editar = () => {
+
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const navigate = useNavigate();
+  const onSubmit = async data => {
+     await Api.post("/user", data);
+    
+
+    navigate('/');
+  }
+
+  return (
+    <div>
+      <img id="teste" src={Background} alt="" />
+
+      <Container className='cada'>
+        <img className='logo' src={Logo} alt="Logo-Pagina-Login" />
+        <h5 className='login'>Editar</h5>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group className="mb-3" controlId="formBasicEmail" style={{ padding: "1% 15%" }}>
+            <Form.Control name="name" {...register('name', { required: true })} type="text" placeholder="nome" style={{ textAlign: "center" }} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail" style={{ padding: "1% 15%" }}>
+            <Form.Control name="email" {...register('email', { required: true })} type="email" placeholder="email" style={{ textAlign: "center" }} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail" style={{ padding: "1% 15%" }}>
+            <Form.Control name="password" {...register('password', { required: true })} type="password" placeholder="senha" style={{ textAlign: "center" }} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail" style={{ padding: "1% 15%" }}>
+            <Form.Control name="password" type="password" placeholder="confirmar senha" style={{ textAlign: "center" }} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail" style={{ padding: "1% 15%" }}>
+            <Form.Control name="apartment" {...register('apartment', { required: true })} type="number" placeholder="unidade/apartamento" style={{ textAlign: "center" }} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword" style={{ padding: "1% 15%" }}>
+            <Form.Control type="password" placeholder="link da foto" style={{ textAlign: "center" }} />
+          </Form.Group>
+          <Button className='botao' type="submit" style={{ background: "#76BB4C", padding: "1% 29%", color: "black" }}>
+            entrar
+          </Button>
+        </Form>
+
+
+      </Container>
+
+    </div>
+
+  )
+}
+
+export default Editar
