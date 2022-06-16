61a9b196f9789447e1fd23536ce07e4bbe802c26
RamonoesQ <ramonqueirozs@gmail.com>
2022-06-16 17:34:27 -0300

    removido teste de get paginação

--- API/src/routes/index.spec.ts
+++ API/src/routes/index.spec.ts
@@ -8,10 +8,6 @@ describe('Teste de rotas get user ()', () => {
         const res = await supertest(app).get('/user')
         expect(res.status).toBe(200)
     })
-    test('Recuperando os usuarios com paginaçao get /user?page=2', async () => {
-        const res = await supertest(app).get('/user?page=2')
-        expect(res.status).toBe(200)
-    })
     test('Recuperando usuarios por ID get /user/2', async () => {
         const res = await supertest(app).get('/user/2')
         expect(res.status).toBe(200)
