770a43d805f1314d5538f9937e02a9c12f9271ea
RamonoesQ <ramonqueirozs@gmail.com>
2022-06-16 20:33:48 -0300

    Revert "corrigindo o index"
    This reverts commit 42560a8a68ba35de258200d020e6dca940d33b14.

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,17 +1,13 @@
 import express from "express";
 import handleMiddleware from './middlewares/handleMiddleware';
-import login from './routes/loginRoutes'
-import post from './routes/postRoutes'
-import user from './routes/userRoutes'
+import {routes} from './routes';
 import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(login);
-app.use(post);
-app.use(user);
+app.use(routes);
 app.use(handleMiddleware);
 
 
