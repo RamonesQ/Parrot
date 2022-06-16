ed22e1eafae1ab5e3322a5dcc3efc5a1ece962f7
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-06-15 21:42:14 -0300

    Liberação da autenticação provisorio

--- API/src/routes/index.ts
+++ API/src/routes/index.ts
@@ -9,17 +9,17 @@ const Auth = require('../middlewares/auth');
 
 export const routes = express.Router();
 
-routes.get('/user', Auth, UsersController.getAllUser);
-routes.get('/user/:id', Auth, UserValidation.getOne, UsersController.getUserById);
+routes.get('/user', UsersController.getAllUser);
+routes.get('/user/:id', UserValidation.getOne, UsersController.getUserById);
 routes.post('/user', UserValidation.create, UsersController.postUser);
-routes.put('/user/:id', Auth, UserValidation.update, UsersController.putUser);
+routes.put('/user/:id', UserValidation.update, UsersController.putUser);
 routes.delete('/user/:id', Auth, UserValidation.destroy, UsersController.deleteUser)
 
-routes.get('/post', Auth, PostController.getAllPosts);
-routes.get('/post/:id', Auth, PostValidation.getOne, PostController.getPostsById);
-routes.post('/post', Auth, PostValidation.create, PostController.postPosts);
-routes.put('/post/:id', Auth, PostValidation.update, PostController.putPosts);
-routes.delete('/post/:id', Auth, PostValidation.destroy, PostController.deletePost);
+routes.get('/post', PostController.getAllPosts);
+routes.get('/post/:id', PostValidation.getOne, PostController.getPostsById);
+routes.post('/post', PostValidation.create, PostController.postPosts);
+routes.put('/post/:id', PostValidation.update, PostController.putPosts);
+routes.delete('/post/:id', PostValidation.destroy, PostController.deletePost);
 
 routes.post('/login', AuthControler.login);
 
