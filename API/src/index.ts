42560a8a68ba35de258200d020e6dca940d33b14
RamonoesQ <ramonqueirozs@gmail.com>
2022-06-16 19:56:31 -0300

    corrigindo o index

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import handleMiddleware from './middlewares/handleMiddleware';
-import {routes} from './routes';
+import login from './routes/loginRoutes'
+import post from './routes/postRoutes'
+import user from './routes/userRoutes'
 import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(routes);
+app.use(login);
+app.use(post);
+app.use(user);
 app.use(handleMiddleware);
 
 
