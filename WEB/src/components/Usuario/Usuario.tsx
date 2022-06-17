6692d5a4c9aa4ef12f89f032705edda5394d0a6c
unknown <eniofilho76@gmail.com>
2022-06-17 11:04:08 -0300

    Correcao header

--- WEB/src/components/Usuario/Usuario.tsx
+++ WEB/src/components/Usuario/Usuario.tsx
@@ -5,6 +5,8 @@ import Icone from "../../assets/Imagens/Icone.png"
 import "./Usuario.css"
 import Header from '../Header/Header';
 import Person from "../../assets/Imagens/Person.png"
+import { Link } from 'react-router-dom';
+
 
 const Perfil = () => {
   return (
@@ -14,35 +16,51 @@ const Perfil = () => {
 
       <Card style={{ width: '30rem', marginLeft: "35%", marginTop: "0%" }}>
         <Card.Body>
-          <div className='userscont'>
-            
-            <img src={Person} alt="Imagem Cliente" />
-            
-            <div className='users'>
-              <div className='usertext'>
-                <p>Nome Sobrenome</p>
-                <p>Ap 82  </p>
-                <p>user@user </p>
-                <p>00 publicações</p>
+
+        <div className='users'>
+              <img src={Person} alt="" />
+              {/* <Card.Title>Card Title</Card.Title> */}
+              <div className='users'>
+                <div className='usertext'>
+                  <p>Nome Sobrenome</p>
+                  <p>Ap  82</p>
+                  <p> User</p>
+                  <p> Publicacoes </p>
+                </div>
+                <div className='editar'>
+                <Link to={`/editar/`} className="btn btn-primary" id='userButton'>
+                    <i className="fa fa-edit">Editar</i>
+                  </Link>
+                  
+                </div>
               </div>
 
-              <div className='editar'>
-                <button type='button' id="editbutton">editar perfil</button>
-              </div>
             </div>
-
-          </div>
+          
 
         </Card.Body>
       </Card>
-      <Card style={{ width: '30rem', marginLeft: "35%", padding: "10% 5% 7%" }}>
+      <Card style={{ width: '30rem', marginLeft: "35%", padding: "0% 0% 0%" }}>
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-          <Card.Text >
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
+
+        <div className='users'>
+              <img src={Person} alt="" />
+              {/* <Card.Title>Card Title</Card.Title> */}
+              <div className='users'>
+                <div className='usertext'>
+                  <p>nome - ap</p>
+                  <p>data</p>
+                  <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius doloribus libero corrupti provident? Quae, asperiores aspernatur quasi laudantium dicta deleniti sequi magni earum similique nostrum ipsa. Veniam cupiditate aut soluta.
+                  </p>
+                </div>
+                <div className='editar'>
+                
+                  
+                </div>
+              </div>
+
+            </div>
+        
         </Card.Body>
       </Card>
 
